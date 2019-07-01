f8660d981b696acfdc13a758af116355c1d9801b
Blake Sager <blake.sager@activeprospect.com>
2019-07-01 17:15:10 -0500

    finish quiz

--- 01_quiz/main.go
+++ 01_quiz/main.go
@@ -6,15 +6,15 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
 	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
-
+	timeLimit := flag.Int("limit", 30, "the time limit for the quiz in seconds")
 	flag.Parse()
 
 	file, err := os.Open(*csvFilename)
-
 	if err != nil {
 		exit("Failed to open CSV file: %s\n")
 	}
@@ -26,14 +26,27 @@ func main() {
 	}
 
 	problems := parseLines(lines)
-
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
 	correct := 0
+
+problemloop:
 	for i, p := range problems {
-		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
-		if answer == p.a {
-			correct++
+		fmt.Printf("Problem #%d: %s = ", i+1, p.q)
+		answerChan := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerChan <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Println()
+			break problemloop
+		case answer := <-answerChan:
+			if answer == p.a {
+				correct++
+			}
 		}
 	}
 
